745ea1549194229251aba765ed75e9e3d25001d5
NikVolf <nikvolf@gmail.com>
2020-02-08 23:48:16 +0300

    basic block prod

--- bin/node/testing/benches/import.rs
+++ bin/node/testing/benches/import.rs
@@ -33,10 +33,29 @@
 
 use node_primitives::Block;
 use node_testing::client::{Client, Backend, Transaction};
+use node_testing::keyring::*;
 use sc_client_db::PruningMode;
 use sc_executor::{NativeExecutor, RuntimeInfo, WasmExecutionMethod, Externalities};
-use sp_consensus::block_import::{BlockImport, BlockImportParams};
+use sp_consensus::{SelectChain, BlockOrigin, BlockImport, BlockImportParams, ForkChoiceStrategy};
 use criterion::{BatchSize, Criterion, criterion_group, criterion_main};
+use sp_runtime::{
+	generic::BlockId,
+	OpaqueExtrinsic,
+	traits::{Block as BlockT, Zero},
+};
+use codec::{Decode, Encode};
+use node_runtime::{
+	Call,
+	CheckedExtrinsic,
+	constants::currency::DOLLARS,
+	UncheckedExtrinsic,
+	MinimumPeriod,
+	BalancesCall,
+};
+use sp_core::ExecutionContext;
+use sp_api::ProvideRuntimeApi;
+use sp_block_builder::BlockBuilder;
+use sp_inherents::InherentData;
 
 criterion_group!(benches, bench_block_import);
 criterion_main!(benches);
@@ -45,12 +64,15 @@ fn genesis() -> node_runtime::GenesisConfig {
 	node_testing::genesis::config(false, Some(node_runtime::WASM_BINARY))
 }
 
+fn sign(xt: CheckedExtrinsic, genesis_hash: [u8; 32], version: u32) -> UncheckedExtrinsic {
+	node_testing::keyring::sign(xt, version, genesis_hash)
+}
+
 // This should return client that is doing everything that full node
 // is doing.
 //
 // - This client should not cache anything.
 //     (TODO: configure zero rocksdb block cache)
-//
 // - This client should use best wasm execution method.
 // - This client should work with real database only.
 fn bench_client() -> (Client, std::sync::Arc<Backend>) {
@@ -79,7 +101,71 @@ fn bench_client() -> (Client, std::sync::Arc<Backend>) {
 // Full (almost) block generation. This is expected to be roughly
 // equal to the block which is hitting weight limit.
 fn generate_block_import(client: &Client) -> BlockImportParams<Block, Transaction> {
-	unimplemented!();
+	let version = client.runtime_version_at(&BlockId::number(0))
+		.expect("There should be runtime version at 0")
+		.spec_version;
+	let genesis_hash = client.block_hash(Zero::zero())
+		.expect("Database error?")
+		.expect("Genesis block always exists; qed")
+		.into();
+
+	let mut block = client
+		.new_block(Default::default())
+		.expect("Block creation failed");
+
+	let timestamp = 1 * MinimumPeriod::get();
+
+	let mut inherent_data = InherentData::new();
+	inherent_data.put_data(sp_timestamp::INHERENT_IDENTIFIER, &timestamp)
+		.expect("Put timestamb failed");
+	inherent_data.put_data(sp_finality_tracker::INHERENT_IDENTIFIER, &0)
+		.expect("Put finality tracker failed");
+
+	for extrinsic in client.runtime_api()
+		.inherent_extrinsics_with_context(
+			&BlockId::number(0),
+			ExecutionContext::BlockConstruction,
+			inherent_data,
+		).expect("Get inherents failed")
+	{
+		block.push(extrinsic).expect("Push inherent failed");
+	}
+
+	let nonce = 0u32;
+
+	let signed = sign(
+		CheckedExtrinsic {
+			signed: Some((alice(), signed_extra(nonce, 1*DOLLARS))),
+			function: Call::Balances(
+				BalancesCall::transfer(
+					pallet_indices::address::Address::Id(bob()),
+					1*DOLLARS
+				)
+			),
+		}, genesis_hash, version);
+	let encoded = Encode::encode(&signed);
+
+	let opaque = OpaqueExtrinsic::decode(&mut &encoded[..])
+		.expect("Failed  to decode opaque");
+
+	block.push(opaque).expect("Push transaction failed");
+
+	let block = block.build().expect("Block build failed").block;
+
+	BlockImportParams {
+		origin: BlockOrigin::File,
+		header: block.header().clone(),
+		post_digests: Default::default(),
+		body: Some(block.extrinsics().to_vec()),
+		storage_changes: Default::default(),
+		finalized: false,
+		justification: Default::default(),
+		auxiliary: Default::default(),
+		intermediates: Default::default(),
+		fork_choice: Some(ForkChoiceStrategy::LongestChain),
+		allow_missing_state: false,
+		import_existing: false,
+	}
 }
 
 // Import generated block.
@@ -90,5 +176,6 @@ fn import_block(client: &mut Client) {
 }
 
 fn bench_block_import(c: &mut Criterion) {
-	unimplemented!()
+	let (mut client, backend) = bench_client();
+	import_block(&mut client);
 }
--- client/api/src/execution_extensions.rs
+++ client/api/src/execution_extensions.rs
@@ -55,7 +55,7 @@ impl Default for ExecutionStrategies {
 		ExecutionStrategies {
 			syncing: ExecutionStrategy::NativeElseWasm,
 			importing: ExecutionStrategy::NativeElseWasm,
-			block_construction: ExecutionStrategy::AlwaysWasm,
+			block_construction: ExecutionStrategy::NativeWhenPossible,
 			offchain_worker: ExecutionStrategy::NativeWhenPossible,
 			other: ExecutionStrategy::NativeElseWasm,
 		}
